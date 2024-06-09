12c4263bdb64dcc4a68a3bef3ae25fa575f7e769
Dachi Natenadze <dachinatenadzeb@gmail.com>
2024-06-09 08:53:01 -0700

    done did all functionality works like a charm

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -7,7 +7,12 @@ interface ProfInfo {
   Name: string;
   User: string;
   Location: string;
-  Links: string;
+  Links: {
+    html_url: string;
+    followers_url: string;
+    following_url: string;
+    repos_url: string;
+  };
   Join: string;
   Description: string;
   ProfilePic: string;
@@ -22,7 +27,12 @@ export default function Profile() {
     Name: "",
     User: "",
     Location: "",
-    Links: "",
+    Links: {
+      html_url: "",
+      followers_url: "",
+      following_url: "",
+      repos_url: "",
+    },
     Join: "",
     Description: "",
     ProfilePic: "",
@@ -37,8 +47,19 @@ export default function Profile() {
           Repos: data.public_repos,
           Followers: data.followers,
           Following: data.following,
+          Name: data.name || data.login,
+          User: data.login,
+          Location: data.location || "",
+          Links: {
+            html_url: data.html_url,
+            followers_url: data.followers_url,
+            following_url: data.following_url,
+            repos_url: data.repos_url,
+          },
+          Join: data.created_at,
+          Description: data.bio || "",
+          ProfilePic: data.avatar_url,
         });
-        console.log(data);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -57,10 +78,29 @@ export default function Profile() {
         <div className="profile">
           <input type="text" value={user} onChange={handleInputChange} />
           <div>
-            <h1></h1>
+            <h1>{profInfo.Name}</h1>
+            <img src={profInfo.ProfilePic} alt="Profile" />
             <p>Repos: {profInfo.Repos}</p>
             <p>Followers: {profInfo.Followers}</p>
             <p>Following: {profInfo.Following}</p>
+            <p>Description: {profInfo.Description}</p>
+            <p>Location: {profInfo.Location}</p>
+            {/* Add links */}
+            <p>
+              GitHub: <a href={profInfo.Links.html_url}>{profInfo.User}</a>
+            </p>
+            <p>
+              Followers:{" "}
+              <a href={profInfo.Links.followers_url}>{profInfo.Followers}</a>
+            </p>
+            <p>
+              Following:{" "}
+              <a href={profInfo.Links.following_url}>{profInfo.Following}</a>
+            </p>
+            <p>
+              Repositories:{" "}
+              <a href={profInfo.Links.repos_url}>{profInfo.Repos}</a>
+            </p>
           </div>
         </div>
       </main>
